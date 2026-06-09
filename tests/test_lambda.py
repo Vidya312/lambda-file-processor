8570368f3ff2f01c3fa8afbdc2dfdad7b1aaa9f6
Vidya312 <vidyaga312@gmail.com>
2026-06-10 00:01:40 +0530

    Update test event data in test_lambda.py

--- tests/test_lambda.py
+++ tests/test_lambda.py
@@ -2,8 +2,8 @@ from lambda_function import lambda_handler
 
 def test_lambda():
     event = {
-        "bucket": "test-bucket",
-        "file": "test.csv"
+        "bucket": "filepipeline-092042969531-raw",
+        "file": "test.txt.txt"
     }
 
     result = lambda_handler(event, None)
